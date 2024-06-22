b07f51ccd8a9276c16e08e360c5a25b6c0c76b42
AlexFlorenco <aflorenco13@gmail.com>
2024-06-22 15:41:07 -0300

    feat: Add new pages for session management

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,7 +1,12 @@
 import 'package:artes/pages/check_page.dart';
+import 'package:artes/pages/create_session_page.dart';
+import 'package:artes/pages/discover_page.dart';
 import 'package:artes/pages/drawing_page.dart';
+import 'package:artes/pages/enter_session_page.dart';
 import 'package:artes/pages/home_page.dart';
 import 'package:artes/pages/loading_page.dart';
+import 'package:artes/pages/player_2_loading.dart';
+import 'package:artes/pages/schoose_session_page.dart';
 import 'package:artes/pages/times_over_page.dart';
 import 'package:flutter/material.dart';
 
@@ -17,8 +22,16 @@ class AppWidget extends StatelessWidget {
       ),
       routes: {
         '/': (context) => const HomePage(),
-        '/loading': (context) => const LoadingPage(),
+        '/schooseSession': (context) => const SchooseSessionsPage(),
+
+        '/createSession': (context) => const CreateSessionPage(),
+        '/player1loading': (context) => const Player1LoadingPage(),
+        
+        '/joinSession': (context) => const JoinSessionPage(),
+        '/player2loading': (context) => const Player2Loading(),
+        
         '/drawing': (context) => DrawingPage(),
+        '/discoverPage': (context) => const DiscoverPage(),
         '/timesOver': (context) => const TimesOverPage(),
         '/check': (context) => const CheckPage(),
       },
