1155569baee00a67491a0798435c58285732ac23
AlexFlorenco <aflorenco13@gmail.com>
2024-06-22 15:41:27 -0300

    feat: Support black button color

--- lib/components/neu_text_button_widget.dart
+++ lib/components/neu_text_button_widget.dart
@@ -2,19 +2,28 @@ import 'package:flutter/material.dart';
 import 'package:neubrutalism_ui/neubrutalism_ui.dart';
 
 class NeuTextButtonWidget extends StatelessWidget {
-  const NeuTextButtonWidget(
-      {required this.label, required this.onPressed, super.key});
+  const NeuTextButtonWidget({
+    required this.label,
+    required this.onPressed,
+    this.isBlack = false,
+    super.key,
+  });
   final String label;
   final Function onPressed;
+  final bool isBlack;
 
   @override
   Widget build(BuildContext context) {
     return NeuTextButton(
-        buttonColor: Colors.white,
+        buttonColor: !isBlack ? Colors.white : Colors.black,
         enableAnimation: true,
         text: Text(
           label,
-          style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 22),
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 22,
+            color: !isBlack ? Colors.black : Colors.white,
+          ),
         ),
         onPressed: () {
           onPressed();
